ef94c30d5cae45ee5925c2773acaad98b5d35c00
Selman Cakmakci <selmancakmakci11@gmail.com>
2024-03-12 00:43:05 +0100

    REACT ASSIGNMENT

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -26,9 +26,14 @@ return (
         </div>
         <div className="about">
         <h2>About</h2>
-        <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        <p>I am a frontend developer with a particular interest 
+            in making things simple and automating daily tasks. 
+            I try to keep up with security and best practices, 
+            and am always looking for new things to learn.</p>
         <h2 className="interests">Interests </h2>
-        <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        <p>Food expert. Music scholar. Reader. Internet 
+            fanatic. Bacon buff. Entrepreneur. Travel geek. 
+            Pop culture ninja. Coffee fanatic.</p>
         </div>
     </main>
     <Footer />
